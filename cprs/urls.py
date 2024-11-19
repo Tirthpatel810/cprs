299116fa3a3856517ab7bcb62371a9c03b4dd936
Tirthpatel810 <pateltirth2833@gmail.com>
2024-11-19 17:53:53 +0530

    Initial commit for deployment

--- cprs/urls.py
+++ cprs/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 import cprs_app.views as v1
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('django-administration/', admin.site.urls),
     path('', v1.login, name='login_form'),
 
